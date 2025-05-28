884a134c3300039707ea7b596e823949f2bbb55a
Harish <143000427+Harish-2006@users.noreply.github.com>
2025-05-28 08:16:10 +0530

    Update settings.py

--- custom_idea/settings.py
+++ custom_idea/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-mbpp#sbq0-%vj=i1lyc!^m9ho7ei#hh9%&8iaw%@s#75-#q8(d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# Add this to support Replit
+replit_url = os.environ.get('REPLIT_HOST', '')
+if replit_url:
+    ALLOWED_HOSTS.append(replit_url)
+
 
 
 # Application definition
